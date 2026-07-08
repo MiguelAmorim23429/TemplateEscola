302f591a6e06324b66427c26a6f3e8bea241d906
MiguelAmorim23429 <amorimmiguel@ipvc.pt>
2026-07-08 23:22:08 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/TemplateEscola/', // 👈 ADICIONE ESTA LINHA COM O NOME DO REPOSITÓRIO
+})
